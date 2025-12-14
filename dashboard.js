6293bf021a15ba11cb53007555e61826bf82568d
Abo-Hamed09 <ll3rr506@gmail.com>
2025-12-14 15:53:01 +0300

    Updated mentor emails to PMU domain

--- dashboard.js
+++ dashboard.js
@@ -7,6 +7,33 @@ if (!user) {
 document.getElementById("welcome").innerText =
   "Welcome, " + user;
 
+// Sample mentor data
+const mentors = [
+  { name: "Ahmed", field: "Cybersecurity", email: "ahmed@pmu.edu.sa" },
+  { name: "Saad", field: "Web Development", email: "Saad@pmu.edu.sa" },
+  { name: "Omar", field: "Networking", email: "omar@pmu.edu.sa" }
+];
+
+
+function searchMentor() {
+  const input = document.getElementById("mentorInput").value.toLowerCase();
+  const result = document.getElementById("result");
+
+  const mentor = mentors.find(
+    m => m.name.toLowerCase() === input
+  );
+
+  if (!mentor) {
+    result.innerText = "Mentor not found";
+    return;
+  }
+
+  result.innerText =
+    "Name: " + mentor.name +
+    " | Field: " + mentor.field +
+    " | Email: " + mentor.email;
+}
+
 function logout() {
   localStorage.removeItem("loggedInUser");
   window.location.href = "index.html";
